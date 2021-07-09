c7718a2840508dd830fff3cc25ed36694bec8c1d
eddzhizhpon <eddzhizhpon@gmail.com>
2021-07-08 22:12:27 -0500

    Clean code

--- source_files/main.cpp
+++ source_files/main.cpp
@@ -2,36 +2,12 @@
 
 int main(int argc, char *argv[]){
 
-    setlocale(LC_ALL, "");
-
-    // Mat imagen = imread("../resources_files/textura.png", IMREAD_GRAYSCALE);
-
-    // LBPDescriptor *lbpd = new LBPDescriptor();
-
-    // vector<int> des = lbpd->lbp(imagen);
-
-    // for (int i = 0; i < des.size(); i++) {
-    //     cout << des[i] << ", ";
-    // }
-
-    // cout << endl;
-
     LBPClassifier *lblClassifier = new LBPClassifier();
-    // lblClassifier->train("../resources_files/train");
+    
     lblClassifier->predict("../resources_files/test", "forest");
     cout << endl;
     cout << endl;
     lblClassifier->predict("../resources_files/test", "buildings");
-
-    // lblClassifier->printMap(lblClassifier->OUTPUT_CLASSES);
-
-    // cout << lblClassifier->OUTPUT_CLASSES << endl;
-    // namedWindow("Textura", WINDOW_AUTOSIZE);
-    // imshow("Textura", imagen);
-    // waitKey(0);
-
-    // cv::destroyAllWindows();
-      
 
     return 0;
 }
